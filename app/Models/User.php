c4f5a1776972f472635da736b706a5c24add3b4b
Carine Sery <carine.sery@gmail.com>
2025-03-07 14:21:23 +0100

    feat: Ajoute des user factories dans la database

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
+        'role',
+        'birthdate',
     ];
 
     /**
