0d9470edd7421cf20fd14e1491fb724821b6882c
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-03-07 16:33:18 +0100

    Feat : Ajout du dossier layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-
-    <h1>Welcome</h1>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        @foreach ($users as $user)
-                <span>{{ $user->firstname }}</span> <br>
-        @endforeach
-    </div>
-    
-</body>
-</html>
+<h1>Welcome</h1>
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+    @foreach ($users as $user)
+            <span>{{ $user->firstname }}</span> <br>
+    @endforeach
+</div>
