176298dca5703b8f7d64a4bac11559542298101a
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-03-08 20:47:23 +0100

    feat : ajout dans la nav le bouton connexion et déconnexion

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -37,7 +37,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
  
-            return redirect()->intended('dashboard');
+            return redirect()->intended('/');
         }
         
         // Si l'authentification échoue :
