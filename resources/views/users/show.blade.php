b727396c9856eeacd9cf8337ca5e7353de26e68b
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-04-03 17:53:39 +0200

    fix : styles

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,13 +1,27 @@
 <x-layout :title="$user->firstname">
-    <div class="container mx-auto">
-       Role : {{ $user->role }} <br>
-       Prénom : {{ $user->firstname }} {{ $user->lastname }} <br>
-       Date : {{ $user->birthdate }} <br>
-       Email : {{ $user->email }} <br>
-    </div>
-    <div>
-        <a href="">Modifier</a>
-        <a href="">Supprimer</a>
+    <div class="flex flex-col gap-10 container mx-auto my-4">
+        <a href="{{ route('users') }}" class="w-35 border rounded bg-black py-1 px-10 text-white hover:bg-gray-700"><span>Retour</span></a>
+        <div class="container mx-auto">
+            <h1 class="font-bold text-xl text-lg">{{ $user->firstname }} {{ $user->lastname }} </h1><br>
+            <span>Date de création de compte : {{ $user->created_at }} </span><br>
+            <span>Mise à jour du compte : {{ $user->updated_at }} </span><br>
 
+            <span>Rôle : {{ $user->role }} </span><br>
+           <span>Date de naissance : {{ $user->birthdate }} </span><br>
+           <span>Email : {{ $user->email }} </span><br>
+        </div>
+        <div class="container mx-auto">
+           <h2 class="font-bold text">Historique des achats</h2>
+           <div class="font-light text-s">Rien pour le moment...</div>
+        </div>
+        <div class="flex gap-4 container mx-auto">
+            <a href="#" class="border rounded bg-black py-1 px-10 text-white hover:bg-gray-700"><span class="">Modifier</span></a>
+            <form action="{{ route('users.destroy', $user) }}" method="POST" >
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="border rounded bg-red-500 py-1 px-10 text-white hover:bg-red-700">Supprimer</button>
+            </form>
+        </div>
     </div>
+    
 </x-layout>
