d37824bf9bc986bf7bf3bf591ae4ac742211ce41
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-04-03 12:04:37 +0200

    fix : filter Admin ok

---
+++ resources/views/components/filterAdmin.blade.php
@@ -0,0 +1,21 @@
+<aside class="bg-gray-900 border-r-1 border-gray-400 p-4 text-white">
+    <div class="mb-10">
+        <img src="#" alt="user">
+        <span>NomAdmin</span>
+    </div >
+    <div class="border-y-1">
+        <button class="w-24 py-1 text-white hover:bg-gray-500">Produits</button>
+        <hr>
+        <button class="w-24 py-1 text-white hover:bg-gray-500">Clients</button>
+        <hr>
+        <button class="w-24 py-1 text-white hover:bg-gray-500">Vente</button>
+        <hr>
+        <button class="w-24 py-1 text-white hover:bg-gray-500">Livraison</button>
+        <hr>
+        <button class="w-24 py-1 text-white hover:bg-gray-500">Commande</button>
+        <hr>
+        <button class="w-24 py-1 text-white hover:bg-gray-500">NewsLetter</button>
+        <hr>
+        <button class="w-24 py-1 text-white hover:bg-gray-500">Stoke</button>
+    </div>
+</aside>
