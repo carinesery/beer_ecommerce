b121de8f33c039eb42e1e88985f93c76ee906ba8
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-04-04 14:39:15 +0200

    feat : création page cart.index, controller et route

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,7 @@ Route::get('/users/{user}', [\App\Http\Controllers\UserController::class, 'show'
 Route::get('/users/edit/{user}', [\App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{user}', [\App\Http\Controllers\UserController::class, 'update'])->name('users.update');
 
+Route::get('/cart', [\App\Http\Controllers\CartController::class, 'index'])->name('cart');
+
+
 
