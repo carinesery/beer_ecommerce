d034e766c22b830479f8b4db759b9d9fe05a3406
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-04-05 20:49:11 +0200

    feat : ajout orderController

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.show');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validation des données
+    $request->validate([
+        'user_id' => 'required',
+        'total_price_without_tax' => 'required',
+        'total_price_with_tax' => 'required|number|int',
+        'tax_amount' => 'required|number|int',
+        'status' => 'required|string|in:cart',
+    ]);
+
+    // Création de l'utilisateur (en utilisant l'assignation de masse)
+   
+    Order::create([
+        'user_id' => $request->user_id,
+        'total_price_without_tax' => $request->total_price_without_tax,
+        'total_price_with_tax' =>$request-> total_price_with_tax,
+        'tax_amount' => $request->tax_amount,
+        'status' =>$request->status,
+    ]);
+
+    // Redirection vers la liste des utilisateurs ou autre page souhaitée avec un message de succès
+    return redirect()->route('/')->with('success', 'Utilisateur créé avec succès !');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
