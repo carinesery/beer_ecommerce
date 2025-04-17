7c61a7df099e9cfa1b92453f556e62143ee46221
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-04-17 23:04:58 +0200

    feat : assamblage des tables users, orders et orderItems

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -11,8 +14,35 @@ class CartController extends Controller
      */
     public function index()
     {
-        return view('cart/index');
-    }
+        $userId = auth()->user()->id;
+
+        // dd($userId);
+        // Vérifiez si l'utilisateur est connecté
+        if (!$userId) {
+            return redirect()->route('login')->with('error', 'Vous devez être connecté pour accéder à votre panier.');
+        }
+        $user = User::findOrFail($userId);
+        // dd($user);
+        $orders = $user->orders;
+        // Vérifiez si l'utilisateur a des commandes
+        if ($orders->isEmpty()) {
+            return redirect()->route('products.index')->with('error', 'Votre panier est vide.');
+        }
+        // dd($orders);
+
+        $orderItems = OrderItem::whereHas('order', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        })->get();
+        
+        dd($orderItems);
+        
+        return view('cart/index',[
+            'orders' => $orders,
+            'orderItems' => $orderItems,
+            'user' => $user,
+        ]);
+    
+}
 
     /**
      * Show the form for creating a new resource.
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,5 +1,29 @@
 <x-layout>
     <h1>page panier</h1>
+    @foreach ($orders as $order)
+
+        <div class="grid items-center grid-cols-[_100px_repeat(5,_minmax(100px,_1fr))] gap-1 bg-gray-200 ">
+            <div class="p-4">
+                <img src="" alt="image produit">
+            </div>
+            <div class="p-4">
+                <span>Nom du produit</span>
+            </div>
+            <div class="p-4">
+                <span>Nom variant</span>
+                <span>{{ $order->total_price_with_tax }}</span>
+s
+                
+
+                
+            </div>
+        
+    @endforeach
+    @foreach ($orderItems as $item)
+        <span>{{ $item->order_id }}</span>
+    @endforeach
+
+    <?php dd($user->id); ?>
     <div class="grid items-center grid-cols-[_100px_repeat(5,_minmax(100px,_1fr))] gap-1 bg-gray-200 ">
         <div class="p-4">
             <img src="" alt="image produit">
@@ -9,6 +33,10 @@
         </div>
         <div class="p-4">
             <span>Nom variant</span>
+            <span> {{ $orders->variant_name }}</span>
+        </div>
+            <span> {{ $orders->status }}</span>
+
         </div>
         <div class="p-4">
             <span>Prix</span>
@@ -16,7 +44,7 @@
         <div class="p-4">
             <span>Disponible</span>
         </div>
-        <div class="p-4">
+        <div >
             <label for="number"> Quantité : </label>
                 <input type="number" name="number" id="number" value="0" min="0" max="10" class="border">
         </div>
