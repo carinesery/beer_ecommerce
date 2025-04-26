48680f17750db7078218849e336d605af412bb9b
Carine Sery <carine.sery@gmail.com>
2025-04-26 22:56:43 +0200

    Suppression de son compte par l'utilisateur

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
      // Définir une constante pour les rôles autorisés
      public const ROLES = ['admin', 'customer'];
