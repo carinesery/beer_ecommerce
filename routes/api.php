4678929aa9c42effea944ef4b3114dacb5013b9a
Cédric BOUCHER <cedric.boucher1990@gmail.com>
2025-05-21 16:28:37 +0200

    fix : modification desprotection  routes admin

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,8 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\StripeController;
 
 
- /** POur récupérer ke token d'identification */
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-// // http://127.0.0.1:8000/api/users
-
 Route::post('/register', [UserController::class, 'store']);
 // http://127.0.0.1:8000/api/register
-
-// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-//     $request->fulfill();
-//     return response()->json(['message' => 'Email vérifié !']);
-// })->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
-
-
 
 Route::post('/authenticate', [LoginController::class, 'authenticate']);
 // http://127.0.0.1:8000/api/authenticate
@@ -40,15 +25,6 @@ Route::get('/products', [ProductController::class, 'index']);
 // http://127.0.0.1:8000/api/products
 Route::get('/products/{id}', [ProductController::class, 'show']);
 // // http://127.0.0.1:8000/api/products/{id}
-
-// Exemple de route :
-// Route::post('/products', [ProductController::class, 'store'])->middleware('auth:sanctum');
-// // http://127.0.0.1:8000/api/products
-// Route::put('/products/{id}', [ProductController::class, 'update'])->middleware('auth:sanctum');
-// // http://127.0.0.1:8000/api/products/{id}
-// Route::delete('/products/{id}', [ProductController::class, 'destroy'])->middleware('auth:sanctum');
-// // http://127.0.0.1:8000/api/products/{id}
-
 
 /** Test d'une route api :D */
 Route::get('/test', function () {
